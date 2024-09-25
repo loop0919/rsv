57338c4f4f765b0559a671e9132ef8e6239d979c
loop0919 <ruupu2001@gmail.com>
2024-09-25 02:34:08 +0000

    スプリント3 Day1

--- reserve_py/controllers/login.py
+++ reserve_py/controllers/login.py
@@ -10,13 +10,12 @@ def login():
     password = request.form.get("password")
     remember = True if request.form.get("remember") else False
     
-    user = service.getUser()
+    user = service.getUser(user_id)
     
     if not user or not service.check_valid_user(user.password, password):
         return redirect(url_for("auth.login"), error="ユーザー名またはパスワードが違います")
     
-    if user.getRole() == "teacher":
+    if user.getRole() == 0:
         return redirect(url_for("teacher.list"))
     else:
         return redirect(url_for("student.list"))
-
